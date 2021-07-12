1fe4c6fa2b1773c283cc759a056c8a4f1ee0b4d9
JingyiNiu <niu.jy1203@gmail.com>
2021-07-12 13:01:30 +1200

    style home page

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./card.styles.css"
+
+const Card = ({ imgUrl, title, content }) => {
+  return (
+    <div className='card'>
+      <div className='card-img'>
+        <img src={imgUrl} alt='image' />
+      </div>
+      <div className='card-title'>{title}</div>
+      <div className='card-content'>{content}</div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/homepage/home.component.jsx
+++ src/pages/homepage/home.component.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Card from "../../components/card/card.component";
 
 import "./home.styles.css";
 
 const Home = () => {
-  return <div>Home Page</div>;
+
+  const url='https://images.unsplash.com/photo-1465056836041-7f43ac27dcb5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80';
+  const title='This is title';
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam error ratione ducimus quisquam ad magnam eveniet aliquam quam, laborum molestias quidem minima commodi hic quo dignissimos natus tempora nulla dolores";
+
+  return (
+    <div className='home'>
+      <Card
+        imgUrl={url}
+        title={title}
+        content={text}
+      />
+      <Card
+        imgUrl={url}
+        title={title}
+        content={text}
+      />
+    </div>
+  );
 };
 
 export default Home;
