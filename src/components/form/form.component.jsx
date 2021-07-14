f4a41aa3dc773a2f6f3567965a6f232885f82f0b
JingyiNiu <niu.jy1203@gmail.com>
2021-07-14 12:04:50 +1200

    install react-hook-form and validate name inputs

--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,101 +1,71 @@
-import React, { useState, useEffect } from "react";
-import Input from "../input/input.component";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 import "./form.styles.css";
 
 const Form = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [dob, setDob] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  const handleFirstName = (e) => {
-    const fname = e.target.value;
-    setFirstName(fname);
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
   };
 
-  const handleLastName = (e) => {
-    const lname = e.target.value;
-    setLastName(lname);
-  };
-
-  const handleEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
-
-  const handlePhone = (e) => {
-    const phone = e.target.value;
-    setPhone(phone);
-  };
-
-  const handleDob = (e) => {
-    const dob = e.target.value;
-    setDob(dob);
-  };
-
-  console.log("firstName: " + firstName);
-  console.log("lastName: " + lastName);
-  console.log("email: " + email);
-  console.log("phone: " + phone);
-  console.log("dob: " + dob);
+  console.log(watch("example"));
 
   return (
-    <div className='form'>
+    <form className='form' onSubmit={handleSubmit(onSubmit)}>
       <div className='form-title'>Please Fill In Your Details</div>
-      <div className='inputs'>
-        <Input
-          text='First Name'
-          placeholder='Your first name'
-          inputType='text'
-          isCorrect={true}
-          errorMsg='please enter valid first name'
-          onChange={handleFirstName}
-        />
-        <Input
-          text='Last Name'
-          placeholder='Your last name'
-          inputType='text'
-          isCorrect={true}
-          errorMsg='please enter valid last name'
-          onChange={handleLastName}
-        />
-        <Input
-          text='Email'
-          placeholder='xxx@xxx.xxx'
-          inputType='email'
-          isCorrect={false}
-          errorMsg='please enter valid email'
-          onChange={handleEmail}
-        />
-        <Input
-          text='Phone Number'
-          placeholder='021 12345'
-          inputType='tel'
-          isCorrect={true}
-          errorMsg='please enter valid phone number'
-          onChange={handlePhone}
-        />
-        <Input
-          text='Date of Birth'
-          inputType='date'
-          isCorrect={false}
-          errorMsg='please choose valid date'
-          onChange={handleDob}
-        />
-        <div className='input-container'>
-          <div className='input-label'>Comments</div>
-          <textarea
-            name='comments'
-            className='input textarea'
-            placeholder='Enter your comments here...'
+      <div className='form-inputs'>
+        <div className='inputs-container'>
+          {/* First Name */}
+          <div className='label'>First Name</div>
+          <input
+            className='input'
+            {...register("firstName", {
+              required: true,
+              maxLength: 20,
+              pattern: /^[A-Za-z]+$/i,
+            })}
           />
+          {errors?.firstName?.type === "required" && (
+            <p className='message'>This field is required</p>
+          )}
+          {errors?.firstName?.type === "maxLength" && (
+            <p className='message'>First name cannot exceed 20 characters</p>
+          )}
+          {errors?.firstName?.type === "pattern" && (
+            <p className='message'>Alphabetical characters only</p>
+          )}
+
+          {/* Last Name */}
+          <div className='label'>Last Name</div>
+          <input
+            className='input'
+            {...register("lastName", {
+              required: true,
+              maxLength: 20,
+              pattern: /^[A-Za-z]+$/i,
+            })}
+          />
+          {errors?.lastName?.type === "required" && (
+            <p className='message'>This field is required</p>
+          )}
+          {errors?.lastName?.type === "maxLength" && (
+            <p className='message'>First name cannot exceed 20 characters</p>
+          )}
+          {errors?.lastName?.type === "pattern" && (
+            <p className='message'>Alphabetical characters only</p>
+          )}
+
+          <input className='button' type='submit' />
         </div>
-        <div className='button'>Submit</div>
-        <div className='button btn-gray'>Back</div>
       </div>
-    </div>
+    </form>
   );
 };
 
---
+++ src/components/input-name/input-name.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputName = () => {
+  return <div></div>;
+};
+
+export default InputName;
